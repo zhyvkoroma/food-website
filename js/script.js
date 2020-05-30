e42cc195b0e41b4643598401f3f39970204292a5
Roma <zhivkoroma@gmail.com>
2020-05-30 18:05:47 +0300

    Получаємо данi з сервера(json-server)

--- js/script.js
+++ js/script.js
@@ -176,34 +176,22 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        ".menu .container"
-    ).render();
+    const getResourse = async (url) => {
+        const res = await fetch(url);
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        14,
-        ".menu .container"
-    ).render();
+        if(!res.ok){
+           throw new Error (`Could not fentch ${url}, status ${res.status}`);
+        }
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        21,
-        ".menu .container"
-    ).render();
+        return await res.json();
+    };
 
-
+    getResourse('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => { //дуструктиризація
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        });
     //modalThanks
     const showThanksModal = message => {
         const prevModalDialog = document.querySelector('.modal__dialog');
@@ -239,7 +227,19 @@ window.addEventListener("DOMContentLoaded", () => {
         failure : "Ooppps.."
     };
 
-    const postData = form => {
+    const postData = async (url, data) => {
+        const res = await fetch(url,  {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data
+    });
+
+    return await res.json();
+};
+
+    const bindPostData = form => {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -250,20 +250,17 @@ window.addEventListener("DOMContentLoaded", () => {
                 margin: 0 auto;
             `;
             form.insertAdjacentElement('afterend', statusMessage);
+
             const formData = new FormData(form);
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
             const object = {};
             formData.forEach(function(value, key){
                 object[key] = value;
             });
-
-            fetch('server.php', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            }).then(data => {
+                ///Json-sever
+            postData('http://localhost:3000/requests', json)
+            .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
                 statusMessage.remove();
@@ -277,10 +274,7 @@ window.addEventListener("DOMContentLoaded", () => {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res));
 });
