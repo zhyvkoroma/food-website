80bf7815ff9a883549ffb84cc8c5bbe7518b1494
zhyvkoroma <60620819+zhyvkoroma@users.noreply.github.com>
2020-06-08 16:06:00 +0300

    Update slider.js

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -48,7 +48,7 @@ function slider() {
         margin-right: 15%;
         margin-left: 15%;
         list-style: none;
-    `; // Если хотите - добавьте в стили, но иногда у нас нет доступа к стилям
+    `; 
     slider.append(indicators);
 
     for (let i = 0; i < slides.length; i++) {
